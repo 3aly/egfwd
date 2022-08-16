c3c32f7c4e34abc15ada30540c252976bbc2f93f
3aly <thebigali1998@gmail.com>
2022-08-16 14:05:18 +0200

    Js all done

--- js/app.js
+++ js/app.js
@@ -18,91 +18,100 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
-*/
-
-const sections= document.querySelectorAll('section');
-const navs=document.querySelectorAll('nav .container ul li');
-const navLinks=document.querySelectorAll('nav .container ul li a');
 
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+const sections= document.querySelectorAll('section'); //get all the sections
+const navBarList=document.getElementById('navbar__list'); //get the navbar list
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
+buildNav(); //build the navbar
 
 
-// Add class 'active' to section when near top of viewport
+const navs=document.querySelectorAll('nav .container ul li'); //get all the li elements in the navbar after its built
+const navLinks=document.querySelectorAll('nav .container ul li a'); //get all links in navbar
+
+navHighlighter();
+scrollToSection();
 
 
-// Scroll to anchor ID using scrollTO event
+//**************main functions**************// 
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+function buildNav(){
+    sections.forEach(section=>{
 
-// Build menu 
+        const newList = document.createElement('li'); //create a new li element
+        newList.className=section.getAttribute('id'); //set the class of the new li element to the id of the section
+        const newLink = document.createElement('a'); //create a new link element
+        newLink.setAttribute('href', '#'+section.getAttribute('id')); //set the href of the new link element to the id of the section
+        newLink.textContent=section.getAttribute('id'); //set the text content of the new link element to the id of the section
+        newList.appendChild(newLink); //append the new link element to the new li element
+        navBarList.appendChild(newList); //append the new li element to the navbar list
+        
+    });
 
-// Scroll to section on link click
-
-navLinks.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault(); // prevent default behaviour of the link
-        const target=e.target.getAttribute('href'); // get the href of the link
-        const targetSection=document.querySelector(target); // get the section that the link points to
-        targetSection.scrollIntoView({behavior:'smooth'}); // scroll to the section
-    }
-    )
- 
+    return navBarList; //return the navbar list
+}
 
 
-});
 
-// Set sections as active
-window.addEventListener('scroll',()=> {
-
-  let current="";
+function scrollToSection(){
 
 
-  
-    for(let section of sections){
-
-        const secttionTop = section.offsetTop; //calculate the offset form each section to the top of the window
-        const sectionHeight=section.clientHeight; //calculate the height of each section
-        if(pageYOffset>secttionTop-sectionHeight/2){
-            current=section.getAttribute('id'); //get the id of the current section
+    navLinks.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault(); // prevent default behaviour of the link
+            const target=e.target.getAttribute('href'); // get the href of the link
+            const targetSection=document.querySelector(target); // get the section that the link points to
+            targetSection.scrollIntoView({behavior:'smooth'}); // scroll to the section
         }
-    }
-    console.log(current);
-    navs.forEach(li=>{
-
-    li.classList.remove('active'); //remove the active class from all the li elements
-  
-    if(li.classList.contains(current)){
-      
-      li.classList.add('active'); //add the active class to the current li element
+        )
+     
+    
+    
+    });
     
 
+}
+
+
+
+function navHighlighter(){
+
+
+    
+    window.addEventListener('scroll',()=> {
+    
+        let current="";
+      
+      
+        
+          for(let section of sections){
+      
+              const secttionTop = section.offsetTop; //calculate the offset form each section to the top of the window
+              const sectionHeight=section.clientHeight; //calculate the height of each section
+              if(pageYOffset>=secttionTop-sectionHeight/2){
+                  current=section.getAttribute('id'); //get the id of the current section
+              }
+          }
+          console.log(current);
+          navs.forEach(li=>{ //loop through all the li elements in the navbar
+      
+          li.classList.remove('active'); //remove the active class from all the li elements
+        
+          if(li.classList.contains(current)){ //if the li element contains the current section
+            
+            li.classList.add('active'); //add the active class to the current li element
+          
+      
+          }
+      
+          })
+      
+       
+      })
+      
+    
     }
 
-    })
-
- 
-})
+//**************main functions**************// 
